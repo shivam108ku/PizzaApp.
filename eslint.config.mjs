22921f24bcbef3f9289b557b8997ee37463cdd22
shivam108ku <shivam108ku@gmail.com>
2025-06-06 16:37:06 +0530

    df

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
 
   tseslint.configs.recommendedTypeChecked,
   {
-       ignores: ['dist', 'node_modules','eslint.config.mjs'],
+    ignores: ["dist", "node_modules", "eslint.config.mjs"],
   },
   {
- 
     languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    rules:{
+    rules: {
       // "no-console":"off",
-     
-    }
+    },
   },
-
 );
